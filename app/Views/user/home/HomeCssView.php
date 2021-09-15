62ae8e79815e68529af817d6a6a0f85fc64ce9a4
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-09-15 16:47:18 +0700

    add homecss

---
+++ app/Views/user/home/HomeCssView.php
@@ -0,0 +1,25 @@
+<style>
+  .list-padding {
+    padding-bottom: 8px;
+  }
+
+  .card-text {
+    color: #6e6b7b;
+  }
+
+  .pointer {
+    cursor: pointer;
+  }
+
+  path {
+    fill: #fff;
+  }
+
+  polygon {
+    fill: #fff;
+  }
+
+  circle {
+    fill: #fff;
+  }
+</style>
