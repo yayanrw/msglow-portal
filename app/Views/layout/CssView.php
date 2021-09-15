dd2ff19a7c0532a4993ea4e69d3253287894cc35
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-09-15 16:46:38 +0700

    add cssiew

---
+++ app/Views/layout/CssView.php
@@ -0,0 +1,15 @@
+<!-- Start CSS -->
+<link rel="apple-touch-icon" href="<?= base_url('assets/ico/apple-icon-120.html'); ?>">
+<link rel="shortcut icon" type="image/x-icon" href="https://pixinvent.com/demo/vuexy-html-bootstrap-admin-template/app-assets/images/ico/favicon.ico">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/vendors.min.css'); ?>">
+<link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.min.css'); ?>">
+<link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/bootstrap-extended.min.css'); ?>">
+<link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/colors.min.css'); ?>">
+<link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/components.min.css'); ?>">
+<link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/vertical-menu.min.css'); ?>">
+<link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/form-validation.css'); ?>">
+<link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/authentication.css'); ?>">
+<!-- End CSS-->
