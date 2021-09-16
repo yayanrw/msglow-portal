2d18f9975ff2231a84b7164b7c3ce2909695ce64
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-09-16 17:07:12 +0700

    add auth system

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -2,10 +2,38 @@
 
 namespace App\Controllers;
 
+use App\Models\UsersModel;
+
 class Login extends BaseController
 {
-    public function index()
+    protected $usersModel;
+
+    public function __construct()
+    {
+        $this->usersModel = new UsersModel();
+    }
+    public function Index()
     {
         return view('login/LoginView');
+    }
+
+    public function Auth()
+    {
+        $loginInformation = [
+            'users_email' => $this->request->getVar('users_email'),
+            'users_password' => $this->request->getVar('users_password')
+        ];
+
+        $users = $this->usersModel->find($loginInformation['users_email']);
+
+        if ($users) {
+            if (password_verify($loginInformation['users_password'], $users['users_password'])) {
+                return redirect()->to('/User/Home');
+            } else {
+                echo "NOT OK";
+            }
+        } else {
+            echo 'Email is incorrect';
+        }
     }
 }
