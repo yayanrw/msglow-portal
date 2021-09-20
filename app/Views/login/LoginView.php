f3499ff43b2644afa10ea8e4ca42c9a0de247e96
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-09-20 15:34:22 +0700

    add toastr and validation

--- app/Views/login/LoginView.php
+++ app/Views/login/LoginView.php
@@ -12,6 +12,9 @@
     <title>Msglow Portal</title>
 
     <?= $this->include('Layout/CssView'); ?>
+    <link rel="stylesheet" href="<?= base_url('assets/css/authentication.css'); ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/form-validation.css'); ?>">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
 
 </head>
 <!-- END: Head-->
@@ -49,21 +52,21 @@
                                 <form id="frmLogin" class="auth-login-form mt-2" action="<?= base_url('Login/Auth'); ?>" method="POST">
                                     <div class="mb-1">
                                         <label class="form-label" for="users_email">Email</label>
-                                        <input class="form-control" id="users_email" type="text" name="users_email" placeholder="john@example.com" aria-describedby="users_email" autofocus="" tabindex="1" />
+                                        <input class="form-control" id="users_email" type="text" name="users_email" placeholder="john@example.com" aria-describedby="users_email" autofocus="" tabindex="1" required />
                                     </div>
                                     <div class="mb-1">
                                         <div class="d-flex justify-content-between">
                                             <label class="form-label" for="users_password">Password</label>
-                                            <a href="auth-forgot-password-cover.html">
+                                            <a href="auth-forgot-password-cover.html" hidden>
                                                 <small>Forgot Password?</small>
                                             </a>
                                         </div>
                                         <div class="input-group input-group-merge form-password-toggle">
-                                            <input class="form-control form-control-merge" id="users_password" type="password" name="users_password" placeholder="············" aria-describedby="users_password" tabindex="2" /><span class="input-group-text cursor-pointer"><i data-feather="eye"></i></span>
+                                            <input class="form-control form-control-merge" id="users_password" type="password" name="users_password" placeholder="············" aria-describedby="users_password" tabindex="2" required /><span class="input-group-text cursor-pointer"><i data-feather="eye"></i></span>
                                         </div>
                                     </div>
                                     <div class="mb-1">
-                                        <div class="form-check">
+                                        <div class="form-check hidden">
                                             <input class="form-check-input" id="remember-me" type="checkbox" tabindex="3" />
                                             <label class="form-check-label" for="remember-me"> Remember Me</label>
                                         </div>
@@ -83,12 +86,62 @@
     </div>
     <!-- END: Content-->
 
-    <?= $this->include('Layout/JsView'); ?>
+    <script src="<?= base_url('assets/js/vendors.min.js'); ?>"></script>
+    <script src="<?= base_url('assets/js/jquery.validate.min.js'); ?>"></script>
+    <script src="<?= base_url('assets/js/app-menu.min.js'); ?>"></script>
+    <script src="<?= base_url('assets/js/app.min.js'); ?>"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
     <script>
-        $('#frmLogin').on('submit', function(e) {
-            $('#btnSubmit').prop('disabled', true)
-            $('#btnSubmitLoading').prop('hidden', false)
+        $(function() {
+            'use strict';
+            var pageLoginForm = $('.auth-login-form');
+            if (pageLoginForm.length) {
+                pageLoginForm.validate({
+                    rules: {
+                        'users_email': {
+                            required: true,
+                            email: true
+                        },
+                        'users_password': {
+                            required: true
+                        }
+                    },
+                });
+            }
         });
+
+        $('#frmLogin').on('submit', function(e) {
+            if ($('.auth-login-form').valid()) {
+                $('#btnSubmit').prop('disabled', true)
+                $('#btnSubmitLoading').prop('hidden', false)
+            }
+        });
+
+        toastr.options = {
+            "closeButton": false,
+            "debug": false,
+            "newestOnTop": false,
+            "progressBar": true,
+            "positionClass": "toast-top-center",
+            "preventDuplicates": false,
+            "onclick": null,
+            "showDuration": "300",
+            "hideDuration": "1000",
+            "timeOut": "5000",
+            "extendedTimeOut": "1000",
+            "showEasing": "swing",
+            "hideEasing": "linear",
+            "showMethod": "slideDown",
+            "hideMethod": "slideUp"
+        }
+
+        <?php
+        $session = session();
+        $flashData = $session->getFlashdata('login_notification');
+        if ($flashData) {
+            echo "toastr.error('" . $flashData . "', 'Notifications')";
+        }
+        ?>
     </script>
 
 </body>
