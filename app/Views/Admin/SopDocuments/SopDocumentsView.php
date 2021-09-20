48e7e675eba98ff20050d7bf39e31c09e451e3aa
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-09-21 00:09:35 +0700

    add sop doc

---
+++ app/Views/Admin/SopDocuments/SopDocumentsView.php
@@ -0,0 +1,9 @@
+<?= $this->extend('Layout/Admin/TemplateView'); ?>
+
+<?= $this->section('CustomCss'); ?>
+<link rel="stylesheet" href="<?= base_url('assets/css/page-knowledge-base.min.css'); ?>">
+<?= $this->endSection(); ?>
+
+<?= $this->section('Content'); ?>
+SOP Documents
+<?= $this->endSection(); ?>
