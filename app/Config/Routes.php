f270808b8313a1bee0e806d3c5d6ad159a284888
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-09-20 16:07:10 +0700

    add custom routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -17,7 +17,7 @@ if (file_exists(SYSTEMPATH . 'Config/Routes.php')) {
  * --------------------------------------------------------------------
  */
 $routes->setDefaultNamespace('App\Controllers');
-$routes->setDefaultController('Home');
+$routes->setDefaultController('Login');
 $routes->setDefaultMethod('index');
 $routes->setTranslateURIDashes(false);
 $routes->set404Override();
@@ -32,6 +32,17 @@ $routes->setAutoRoute(true);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Login::index');
+$routes->get('logout', 'Login::Logout');
+$routes->group('admin', ['filter' => 'auth'], function ($routes) {
+    $routes->add('/', 'Admin\Home::index');
+    $routes->add('home', 'Admin\Home::index');
+});
+$routes->group('user', ['filter' => 'auth'], function ($routes) {
+    $routes->add('/', 'User\Home::index');
+    $routes->add('apps', 'User\Apps::index');
+    $routes->add('documentations', 'User\Documentations::index');
+    $routes->add('sopdocuments', 'User\SopDocuments::index');
+});
 
 /*
  * --------------------------------------------------------------------
