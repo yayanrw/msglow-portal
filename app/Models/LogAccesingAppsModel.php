3f7d21ad7366a451f0ff43209c156f8a9ee4f748
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-09-21 00:47:53 +0700

    add logaccessingapps

---
+++ app/Models/LogAccesingAppsModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class LogAccesingAppsModel extends Model
+{
+    protected $table      = 't_log_accesing_apps';
+    protected $primaryKey = 'log_accesing_apps_pid';
+}
