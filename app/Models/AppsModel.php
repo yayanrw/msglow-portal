86dba757acdc0234d6d9839b0ae9835e06caca6d
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-09-21 01:13:15 +0700

    add allowedfield

--- app/Models/AppsModel.php
+++ app/Models/AppsModel.php
@@ -8,4 +8,5 @@ class AppsModel extends Model
 {
     protected $table      = 'm_apps';
     protected $primaryKey = 'apps_pid';
+    protected $allowedFields = ['apps_name', 'apps_subname', 'apps_desc', 'apps_owner', 'apps_url', 'apps_date_release', 'apps_icon', 'apps_banner_img', 'apps_key_session', 'is_active', 'updated_at', 'updated_by'];
 }
