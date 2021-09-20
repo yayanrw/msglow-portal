42218545e1b9b05b13f655e6a1718e76eab6e95d
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-09-20 21:03:35 +0700

    get apps

--- app/Controllers/user/Home.php
+++ app/Controllers/user/Home.php
@@ -3,13 +3,22 @@
 namespace App\Controllers\user;
 
 use App\Controllers\BaseController;
+use App\Models\AppsModel;
 
 class Home extends BaseController
 {
+    protected $appsModel;
+
+    public function __construct()
+    {
+        $this->appsModel = new AppsModel();
+    }
+
     public function index()
     {
         $data = [
-            'title'   => 'Home'
+            'title'   => 'Home',
+            'apps'  => $this->appsModel->findAll()
         ];
         return view('User/Home/HomeView', $data);
     }
