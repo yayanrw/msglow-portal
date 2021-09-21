a86df78a8e919d7ae68d9f2d7379da9d5de93850
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-09-21 09:21:15 +0700

    add insert method

--- app/Controllers/Admin/AppsController.php
+++ app/Controllers/Admin/AppsController.php
@@ -36,17 +36,38 @@ class AppsController extends BaseController
     {
         $data = [
             'title'   => 'Apps Management • Input New Apps',
-            // 'apps'  => $this->appsModel->findAll()
         ];
         return view('Admin/Apps/AppsInputView', $data);
+    }
+
+    public function Insert()
+    {
+        $this->appsModel->insert([
+            'apps_name'         => $this->request->getVar('apps_name'),
+            'apps_subname'      => $this->request->getVar('apps_subname'),
+            'apps_desc'         => $this->request->getVar('apps_desc'),
+            'apps_owner'        => $this->request->getVar('apps_owner'),
+            'apps_url'          => $this->request->getVar('apps_url'),
+            'apps_date_release' => $this->request->getVar('apps_date_release'),
+            'apps_icon'         => $this->request->getVar('apps_icon'),
+            'apps_banner_img'   => $this->request->getVar('apps_banner_img'),
+            'apps_key_session'  => $this->request->getVar('apps_key_session'),
+            'created_by'        => session()->get('users_email')
+        ]);
+        session()->setFlashdata('apps_notification', 'Save successfully');
+        return redirect()->to('admin/apps-management');
     }
 
     public function Active($apps_pid = null)
     {
         $apps = $this->appsModel->find($apps_pid);
+
         $this->appsModel->update($apps_pid, [
             'is_active'   => !$apps['is_active']
         ]);
-        return redirect()->back();
+
+        $status = $apps['is_active'] ? 'Inactive' : 'Active';
+
+        return redirect()->back()->with('apps_notification', $apps['apps_name'] . ' • ' . $apps['apps_subname'] . ' set to ' . $status);
     }
 }
