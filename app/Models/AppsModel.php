5cb3480df198f3815b2035cc677738eae0d9ad8f
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-09-21 14:06:22 +0700

    add created_by allow

--- app/Models/AppsModel.php
+++ app/Models/AppsModel.php
@@ -8,5 +8,5 @@ class AppsModel extends Model
 {
     protected $table      = 'm_apps';
     protected $primaryKey = 'apps_pid';
-    protected $allowedFields = ['apps_name', 'apps_subname', 'apps_desc', 'apps_owner', 'apps_url', 'apps_date_release', 'apps_icon', 'apps_banner_img', 'apps_key_session', 'is_active', 'updated_at', 'updated_by'];
+    protected $allowedFields = ['apps_name', 'apps_subname', 'apps_desc', 'apps_owner', 'apps_url', 'apps_date_release', 'apps_icon', 'apps_banner_img', 'apps_key_session', 'is_active', 'created_by', 'updated_at', 'updated_by'];
 }
