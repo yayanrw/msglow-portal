630774652d6a42447554ef11d983e05b669578af
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-09-21 16:47:41 +0700

    env

--- app/Controllers/LoginController.php
+++ app/Controllers/LoginController.php
@@ -24,7 +24,7 @@ class LoginController extends BaseController
                 return redirect()->to('/user');
             }
         } else {
-            return view('errors\html\error_404');
+            return view('errors/html/error_404');
         }
     }
 
