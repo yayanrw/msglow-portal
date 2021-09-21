b30b68ffc3e5ab6c3bd43c85e8cbe9f844faf997
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-09-21 14:06:14 +0700

    add update apps

--- app/Controllers/Admin/AppsController.php
+++ app/Controllers/Admin/AppsController.php
@@ -54,7 +54,37 @@ class AppsController extends BaseController
             'apps_key_session'  => $this->request->getVar('apps_key_session'),
             'created_by'        => session()->get('users_email')
         ]);
-        session()->setFlashdata('apps_notification', 'Save successfully');
+        session()->setFlashdata('apps_notification', 'Saved successfully');
+        return redirect()->to('admin/apps-management');
+    }
+
+    public function Edit($apps_pid = null)
+    {
+        $apps = $this->appsModel->find($apps_pid);
+
+        $data = [
+            'title'   => 'Apps Management • Edit Apps',
+            'apps'    => $apps
+        ];
+        return view('Admin/Apps/AppsEditView', $data);
+    }
+
+    public function Update()
+    {
+        $this->appsModel->update($this->request->getVar('apps_pid'), [
+            'apps_name'         => $this->request->getVar('apps_name'),
+            'apps_subname'      => $this->request->getVar('apps_subname'),
+            'apps_desc'         => $this->request->getVar('apps_desc'),
+            'apps_owner'        => $this->request->getVar('apps_owner'),
+            'apps_url'          => $this->request->getVar('apps_url'),
+            'apps_date_release' => $this->request->getVar('apps_date_release'),
+            'apps_icon'         => $this->request->getVar('apps_icon'),
+            'apps_banner_img'   => $this->request->getVar('apps_banner_img'),
+            'apps_key_session'  => $this->request->getVar('apps_key_session'),
+            'updated_at'        => date("Y-m-d H:i:s"),
+            'updated_by'        => session()->get('users_email')
+        ]);
+        session()->setFlashdata('apps_notification', 'Updated successfully');
         return redirect()->to('admin/apps-management');
     }
 
@@ -66,8 +96,36 @@ class AppsController extends BaseController
             'is_active'   => !$apps['is_active']
         ]);
 
-        $status = $apps['is_active'] ? 'Inactive' : 'Active';
+        $apps_name = $apps['apps_name'] . ' - ' . $apps['apps_subname'];
+        $message = $apps['is_active'] ? $apps_name . ' successfully deactivated' : 'Aplikasi ' . $apps_name . ' successfully activated';
 
-        return redirect()->back()->with('apps_notification', $apps['apps_name'] . ' • ' . $apps['apps_subname'] . ' set to ' . $status);
+        return redirect()->back()->with('apps_notification', $message);
+    }
+
+    public function ActiveAjax($apps_pid = null)
+    {
+        try {
+            $apps = $this->appsModel->find($apps_pid);
+
+            $this->appsModel->update($apps_pid, [
+                'is_active'   => !$apps['is_active']
+            ]);
+
+            $apps_name = $apps['apps_name'] . ' - ' . $apps['apps_subname'];
+            $message = $apps['is_active'] ? $apps_name . ' successfully deactivated' : 'Aplikasi ' . $apps_name . ' successfully activated';
+
+
+            $data = [
+                'status' => true,
+                'message' => $message,
+            ];
+            return $this->response->setJSON($data);
+        } catch (\Throwable $th) {
+            $data = [
+                'status' => false,
+                'message' => $th,
+            ];
+            return $this->response->setJSON($data);
+        }
     }
 }
