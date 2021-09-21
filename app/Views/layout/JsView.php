3339160bd2d78270d9577dc7b33e60177c25ad63
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-09-21 09:48:38 +0700

    add toastr config

--- app/Views/layout/JsView.php
+++ app/Views/layout/JsView.php
@@ -2,6 +2,7 @@
 <script src="<?= base_url('assets/js/vendors.min.js'); ?>"></script>
 <script src="<?= base_url('assets/js/app-menu.min.js'); ?>"></script>
 <script src="<?= base_url('assets/js/app.min.js'); ?>"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 
 <script>
     $(window).on('load', function() {
@@ -12,5 +13,23 @@
             });
         }
     })
+
+    toastr.options = {
+        "closeButton": false,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": true,
+        "positionClass": "toast-bottom-left",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "slideDown",
+        "hideMethod": "slideUp"
+    }
 </script>
 <!-- End JS -->
