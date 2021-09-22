06eb414670a9e1a220c3af083e481436132e38e9
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-09-22 16:39:12 +0700

    add trycatch

--- app/Controllers/Admin/HomeController.php
+++ app/Controllers/Admin/HomeController.php
@@ -4,26 +4,41 @@ namespace App\Controllers\admin;
 
 use App\Controllers\BaseController;
 use App\Models\AppsModel;
+use App\Models\LogErrorModel;
 
 class HomeController extends BaseController
 {
+    protected $router;
     protected $appsModel;
+    protected $logErrorModel;
 
     public function __construct()
     {
+        $this->router = \Config\Services::router();
         $this->appsModel = new AppsModel();
+        $this->logErrorModel = new LogErrorModel();
     }
 
     public function Index()
     {
-        $data = [
-            'title'   => 'Home',
-            'latest_apps'  => $this->appsModel
-                ->orderBy('apps_date_release desc')
-                ->limit(5)
-                ->get()
-                ->getResult('array')
-        ];
-        return view('admin/home/home_view', $data);
+        try {
+            $data = [
+                'title'   => 'Home',
+                'latest_apps'  => $this->appsModel
+                    ->orderBy('apps_date_release desc')
+                    ->limit(5)
+                    ->get()
+                    ->getResult('array')
+            ];
+            return view('admin/home/home_view', $data);
+        } catch (\Throwable $th) {
+            $this->logErrorModel->InsertLog(
+                $this->router->controllerName(),
+                $this->router->methodName(),
+                $th,
+                session()->get('users_email')
+            );
+            return view('errors/html/production');
+        }
     }
 }
