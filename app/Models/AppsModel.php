3ba07b70376a0ee2d1eb3c48e7e99eb6e8935c0d
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-09-22 19:30:30 +0700

    add join subcateg

--- app/Models/AppsModel.php
+++ app/Models/AppsModel.php
@@ -9,4 +9,9 @@ class AppsModel extends Model
     protected $table      = 'm_apps';
     protected $primaryKey = 'apps_pid';
     protected $allowedFields = ['apps_name', 'apps_subname', 'apps_desc', 'apps_owner', 'apps_url', 'apps_date_release', 'apps_icon', 'apps_banner_img', 'apps_key_session', 'is_active', 'created_at', 'created_by', 'updated_at', 'updated_by'];
+
+    public function AppsWithSubCategory()
+    {
+        return $this->join('t_apps_sub_category', 't_apps_sub_category.apps_pid = m_apps.apps_pid')->get();
+    }
 }
