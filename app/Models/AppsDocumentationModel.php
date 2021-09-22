0d1f3c81c4c84ffe9dec9e52aeb194eb0cfbe987
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-09-22 19:30:10 +0700

    add appsdoc model

---
+++ app/Models/AppsDocumentationModel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class AppsDocumentationModel extends Model
+{
+    protected $table      = 't_apps_document';
+    protected $primaryKey = 'apps_document_pid';
+    protected $allowedFields = ['apps_sub_category_pid', 'apps_document_title', 'apps_document_file', 'apps_document_banner_img', 'created_at', 'created_by', 'updated_at', 'updated_by'];
+}
