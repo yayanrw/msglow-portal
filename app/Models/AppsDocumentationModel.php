8ce09176b76957b44716452725b9b309128b4c06
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-09-23 06:09:58 +0700

    add query

--- app/Models/AppsDocumentationModel.php
+++ app/Models/AppsDocumentationModel.php
@@ -9,4 +9,10 @@ class AppsDocumentationModel extends Model
     protected $table      = 't_apps_document';
     protected $primaryKey = 'apps_document_pid';
     protected $allowedFields = ['apps_sub_category_pid', 'apps_document_title', 'apps_document_file', 'apps_document_banner_img', 'created_at', 'created_by', 'updated_at', 'updated_by'];
+
+    public function AppsDocumentationWithAppsSubCategory()
+    {
+        return $this->join('t_apps_sub_category', 't_apps_sub_category.apps_sub_category_pid = t_apps_document.apps_sub_category_pid')
+            ->join('m_apps', 'm_apps.apps_pid = t_apps_sub_category.apps_pid')->get();
+    }
 }
