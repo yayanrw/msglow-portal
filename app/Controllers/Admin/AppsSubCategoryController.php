ed851a62c231381d6fa9d920d8a3f6f884aa47c5
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-09-22 16:39:03 +0700

    add insert

---
+++ app/Controllers/Admin/AppsSubCategoryController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Controllers\admin;
+
+use App\Controllers\BaseController;
+use App\Models\AppsModel;
+use App\Models\AppsSubCategoryModel;
+use App\Models\LogErrorModel;
+
+class AppsSubCategoryController extends BaseController
+{
+    protected $router;
+    protected $appsSubCategoryModel;
+    protected $appsModel;
+    protected $logErrorModel;
+
+    public function __construct()
+    {
+        $this->router = \Config\Services::router();
+        $this->appsSubCategoryModel = new AppsSubCategoryModel();
+        $this->appsModel = new AppsModel();
+        $this->logErrorModel = new LogErrorModel();
+    }
+
+    public function Input()
+    {
+        try {
+            $data = [
+                'title' => 'Apps Documentation',
+                'subtitle' => 'Input New Sub-Category',
+                'apps' => $this->appsModel->findAll()
+            ];
+            return view('admin/apps_sub_category/sub_category_input_view', $data);
+        } catch (\Throwable $th) {
+            $this->logErrorModel->InsertLog(
+                $this->router->controllerName(),
+                $this->router->methodName(),
+                $th,
+                session()->get('users_email')
+            );
+            return view('errors/html/production');
+        }
+    }
+
+    public function Insert()
+    {
+        try {
+            $this->appsSubCategoryModel->insert([
+                'apps_pid' => $this->request->getVar('apps_pid'),
+                'apps_sub_category_title' => $this->request->getVar('apps_sub_category_title'),
+                'apps_sub_banner_img' => $this->request->getVar('apps_sub_banner_img'),
+                'created_by' => session()->get('users_email')
+            ]);
+            session()->setFlashdata('successMsg', $this->savedSuccessMsg);
+            return redirect()->to('admin/apps-documentation');
+        } catch (\Throwable $th) {
+            $this->logErrorModel->InsertLog(
+                $this->router->controllerName(),
+                $this->router->methodName(),
+                $th,
+                session()->get('users_email')
+            );
+            session()->setFlashdata('errorMsg', $this->errorProcessingMsg);
+            return redirect()->to('admin/apps-management');
+        }
+    }
+}
