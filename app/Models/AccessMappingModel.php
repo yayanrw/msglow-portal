7e72fd01717ba2fa3f8b656a02ab9795236aa60a
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-09-23 15:11:57 +0700

    add accessmapp model

---
+++ app/Models/AccessMappingModel.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class AccessMappingModel extends Model
+{
+    protected $table      = 't_access_mapping';
+    protected $primaryKey = 'access_mapping_pid';
+    protected $allowedFields = ['users_email', 'apps_pid', 'created_by', 'updated_at', 'updated_by'];
+
+    public function UpdateOnUsers($old_email, $new_email)
+    {
+        $update = $this->where('users_email', $old_email)
+            ->get()
+            ->getResult();
+
+        foreach ($update as $key) {
+            $this->update($key->access_mapping_pid, [
+                'users_email' => $new_email,
+                'updated_at'  => date('Y-m-d H:i:s'),
+                'updated_by'  => session()->get('users_email')
+            ]);
+        }
+        return $update;
+    }
+}
