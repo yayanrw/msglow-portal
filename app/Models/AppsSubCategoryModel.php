232b6207d9468910e927c16ea2d88daeb6f3c427
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-09-23 22:12:13 +0700

    add is_active

--- app/Models/AppsSubCategoryModel.php
+++ app/Models/AppsSubCategoryModel.php
@@ -8,5 +8,5 @@ class AppsSubCategoryModel extends Model
 {
     protected $table      = 't_apps_sub_category';
     protected $primaryKey = 'apps_sub_category_pid';
-    protected $allowedFields = ['apps_pid', 'apps_sub_category_title', 'apps_sub_category_banner_img', 'created_at', 'created_by', 'updated_at', 'updated_by'];
+    protected $allowedFields = ['apps_pid', 'apps_sub_category_title', 'apps_sub_category_banner_img', 'is_active', 'created_by', 'updated_at', 'updated_by'];
 }
