833b9c9315582fb992b3024cb45741d7ba6fd30b
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-09-23 15:11:30 +0700

    change pk add method

--- app/Controllers/LoginController.php
+++ app/Controllers/LoginController.php
@@ -36,35 +36,36 @@ class LoginController extends BaseController
             'users_password' => $this->request->getVar('users_password')
         ];
 
-        $users = $this->usersModel->find($loginInformation['users_email']);
+        $users = $this->usersModel->where('users_email', $loginInformation['users_email'])->get()->getRow();
 
         if ($users) {
-            if (password_verify($loginInformation['users_password'], $users['users_password'])) {
+            if (password_verify($loginInformation['users_password'], $users->users_password)) {
                 //store session
                 $sessionData = [
-                    'users_email'      => $users['users_email'],
-                    'users_name'       => $users['users_name'],
-                    'users_division'   => $users['users_division'],
-                    'is_administrator' => $users['is_administrator'],
+                    'users_email'      => $users->users_email,
+                    'users_name'       => $users->users_name,
+                    'users_division'   => $users->users_division,
+                    'is_administrator' => $users->is_administrator,
                     'logged_in'        => true,
                     'last_logged_in'   => date("Y-m-d H:i:s")
                 ];
                 $session->set($sessionData);
 
+
                 //update users
-                $this->usersModel->update($users['users_email'], [
+                $this->usersModel->update($users->users_pid, [
                     'last_login'   => date("Y-m-d H:i:s")
                 ]);
 
                 //insert log
                 $this->logLoginModel->insert([
-                    'users_email'  => $users['users_email'],
+                    'users_email'  => $users->users_email,
                     'device'       => null,
                     'ip'           => null,
                     'location'     => null,
                 ]);
 
-                return $users['is_administrator'] ? redirect()->to('/admin') : redirect()->to('/user');
+                return $users->is_administrator ? redirect()->to('/admin') : redirect()->to('/user');
             } else {
                 $session->setFlashdata('login_notification', 'Your email or password is incorrect');
                 return redirect()->to('/login');
--- app/Models/UsersModel.php
+++ app/Models/UsersModel.php
@@ -7,6 +7,42 @@ use CodeIgniter\Model;
 class UsersModel extends Model
 {
     protected $table      = 'm_users';
-    protected $primaryKey = 'users_email';
+    protected $primaryKey = 'users_pid';
     protected $allowedFields = ['users_name', 'users_email', 'users_division', 'users_password', 'is_administrator', 'last_login', 'ip', 'location', 'device', 'updated_at', 'updated_by'];
+
+    public function UsersWithApps()
+    {
+        return $this->select("m_users.*, GROUP_CONCAT(c.apps_name SEPARATOR ', ') apps_name, GROUP_CONCAT(c.apps_subname SEPARATOR ', ') apps_subname")
+            ->join('t_access_mapping b', 'b.users_email = m_users.users_email')
+            ->join('m_apps c', 'c.apps_pid = b.apps_pid')
+            ->groupBy('m_users.users_email')
+            ->get()
+            ->getResult('array');
+    }
+
+    public function UsersWithAppsRow($users_pid = null)
+    {
+        return $this->select("m_users.*, GROUP_CONCAT(c.apps_name SEPARATOR ', ') apps_name, GROUP_CONCAT(c.apps_subname SEPARATOR ', ') apps_subname")
+            ->join('t_access_mapping b', 'b.users_email = m_users.users_email')
+            ->join('m_apps c', 'c.apps_pid = b.apps_pid')
+            ->groupBy('m_users.users_email')
+            ->where('m_users.users_pid', $users_pid)
+            ->get()
+            ->getRow();
+    }
+
+    public function UpdateUsers($users_pid, $users_email, $users_password)
+    {
+        return $this->update($users_pid, [
+            'users_email' => $users_email,
+            'users_password' => password_hash($users_password, PASSWORD_DEFAULT),
+            'updated_at' => date("Y-m-d H:i:s"),
+            'updated_by' => session()->get('users_email'),
+        ]);
+    }
+
+    public function GetEmail($users_pid = null)
+    {
+        return $this->find($users_pid)['users_email'];
+    }
 }
