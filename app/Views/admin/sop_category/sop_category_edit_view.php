08ae30207ded1fe383730625b0604269096ec2e0
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-09-24 16:09:29 +0700

    add sop cat view

---
+++ app/Views/admin/sop_category/sop_category_edit_view.php
@@ -0,0 +1,51 @@
+<?= $this->extend('layout/admin/template_view'); ?>
+
+<?= $this->section('custom_css'); ?>
+<link rel="stylesheet" href="<?= base_url('assets/css/page-knowledge-base.min.css'); ?>">
+<?= $this->endSection(); ?>
+
+
+<?= $this->section('content'); ?>
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-body p-3">
+                <form class="form form-horizontal" action="<?= base_url('admin/sop-category/update'); ?>" method="post" enctype="multipart/form-data">
+                    <?= csrf_field(); ?>
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="mb-1 row">
+                                <div class="col-sm-3">
+                                    <label class="col-form-label text-black" for="sop_category_title">Category Title</label>
+                                </div>
+                                <div class="col-sm-9">
+                                    <input type="text" id="sop_category_pid" class="form-control text-black" name="sop_category_pid" placeholder="App pid" value="<?= $sop_category['sop_category_pid']; ?>" hidden required>
+                                    <input type="text" id="sop_category_title" class="form-control text-black" name="sop_category_title" placeholder="Category title" value="<?= $sop_category['sop_category_title']; ?>" required>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="mb-1 row">
+                                <div class="col-sm-3">
+                                    <label class="col-form-label text-black" for="sop_category_banner_img">Page Banner Image</label>
+                                </div>
+                                <div class="col-sm-6">
+                                    <input id="sop_category_banner_img" name="sop_category_banner_img" class="form-control" type="file">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row pt-2">
+                            <div class="col-sm-3">
+                                <a href="<?= base_url('admin/sop-documents'); ?>" class="btn btn-outline-secondary waves-effect">Cancel</a>
+                            </div>
+                            <div class="col-sm-9">
+                                <button type="submit" class="btn btn-success me-1 waves-effect waves-float waves-light">Save</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<?= $this->endSection(); ?>
