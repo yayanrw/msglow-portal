f8fb3393caa7868dc3be117711fbb5097e7477df
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-09-24 15:16:37 +0700

    mod sopdoc model

--- app/Models/SopDocumentsModel.php
+++ app/Models/SopDocumentsModel.php
@@ -9,4 +9,21 @@ class SopDocumentsModel extends Model
     protected $table      = 't_sop_documents';
     protected $primaryKey = 'sop_documents_pid';
     protected $allowedFields = ['sop_category_pid', 'sop_documents_title', 'sop_documents_file', 'sop_documents_banner_img', 'is_active', 'created_by', 'updated_at', 'updated_by'];
+
+    public function SopDocumentsWithSopCategory()
+    {
+        return $this->select('t_sop_documents.*, m_sop_category.sop_category_title')
+            ->join('m_sop_category', 'm_sop_category.sop_category_pid = t_sop_documents.sop_category_pid')
+            ->get()
+            ->getResult();
+    }
+
+    public function SopDocumentsWithSopCategoryDetail($sop_documents_pid = null)
+    {
+        return $this->select('t_sop_documents.*, m_sop_category.sop_category_title')
+            ->join('m_sop_category', 'm_sop_category.sop_category_pid = t_sop_documents.sop_category_pid')
+            ->where('t_sop_documents.sop_documents_pid', $sop_documents_pid)
+            ->get()
+            ->getRow();
+    }
 }
