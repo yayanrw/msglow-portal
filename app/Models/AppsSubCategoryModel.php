f0931e58b709201fc15f59b7f9c1fd954473b183
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-09-24 07:11:48 +0700

    add subcat detail

--- app/Models/AppsSubCategoryModel.php
+++ app/Models/AppsSubCategoryModel.php
@@ -9,4 +9,13 @@ class AppsSubCategoryModel extends Model
     protected $table      = 't_apps_sub_category';
     protected $primaryKey = 'apps_sub_category_pid';
     protected $allowedFields = ['apps_pid', 'apps_sub_category_title', 'apps_sub_category_banner_img', 'is_active', 'created_by', 'updated_at', 'updated_by'];
+
+    public function AppsSubCategoryWithAppsDetail($apps_sub_category_pid = null)
+    {
+        return $this->select('t_apps_sub_category.*, m_apps.apps_name, m_apps.apps_subname')
+            ->join('m_apps', 'm_apps.apps_pid = t_apps_sub_category.apps_pid')
+            ->where('t_apps_sub_category.apps_sub_category_pid', $apps_sub_category_pid)
+            ->get()
+            ->getRow();
+    }
 }
