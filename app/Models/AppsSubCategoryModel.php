32ce9b2372d161c0452f8c9861dc0a8b7508c97c
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-09-25 00:38:03 +0700

    add top 5 query

--- app/Models/AppsSubCategoryModel.php
+++ app/Models/AppsSubCategoryModel.php
@@ -10,6 +10,14 @@ class AppsSubCategoryModel extends Model
     protected $primaryKey = 'apps_sub_category_pid';
     protected $allowedFields = ['apps_pid', 'apps_sub_category_title', 'apps_sub_category_banner_img', 'is_active', 'created_by', 'updated_at', 'updated_by'];
 
+    public function AppsSubCategoryWithAppsTop5Latest()
+    {
+        return $this->select('t_apps_sub_category.*, m_apps.apps_name, m_apps.apps_subname')
+            ->join('m_apps', 'm_apps.apps_pid = t_apps_sub_category.apps_pid')
+            ->get()
+            ->getResultArray();
+    }
+
     public function AppsSubCategoryWithAppsDetail($apps_sub_category_pid = null)
     {
         return $this->select('t_apps_sub_category.*, m_apps.apps_name, m_apps.apps_subname')
