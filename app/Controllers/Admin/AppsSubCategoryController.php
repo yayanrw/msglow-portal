fb2db5d2627ea2520df4eb5637823e79bbf82fc2
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-09-24 07:12:08 +0700

    mod subcateg input

--- app/Controllers/Admin/AppsSubCategoryController.php
+++ app/Controllers/Admin/AppsSubCategoryController.php
@@ -30,7 +30,28 @@ class AppsSubCategoryController extends BaseController
                 'subtitle' => 'Input New Sub-Category',
                 'apps' => $this->appsModel->findAll()
             ];
-            return view('admin/apps_sub_category/sub_category_input_view', $data);
+            return view('admin/apps_sub_category/apps_sub_category_input_view', $data);
+        } catch (\Throwable $th) {
+            $this->logErrorModel->InsertLog(
+                $this->router->controllerName(),
+                $this->router->methodName(),
+                $th,
+                session()->get('users_email')
+            );
+            return view('errors/html/production');
+        }
+    }
+
+    public function Edit($apps_sub_category_pid = null)
+    {
+        try {
+            $data = [
+                'title' => 'Apps Documentation',
+                'subtitle' => 'Edit Sub-Category',
+                'apps_sub_category' => $this->appsSubCategoryModel->find($apps_sub_category_pid),
+                'apps' => $this->appsModel->findAll()
+            ];
+            return view('admin/apps_sub_category/apps_sub_category_edit_view', $data);
         } catch (\Throwable $th) {
             $this->logErrorModel->InsertLog(
                 $this->router->controllerName(),
@@ -61,7 +82,56 @@ class AppsSubCategoryController extends BaseController
                 session()->get('users_email')
             );
             session()->setFlashdata('errorMsg', $this->errorProcessingMsg);
-            return redirect()->to('admin/apps-management');
+            return redirect()->to('admin/apps-documentation');
+        }
+    }
+
+    public function Update()
+    {
+        try {
+            $this->appsSubCategoryModel->update($this->request->getVar('apps_sub_category_pid'), [
+                'apps_pid' => $this->request->getVar('apps_pid'),
+                'apps_sub_category_title' => $this->request->getVar('apps_sub_category_title'),
+                'apps_sub_category_banner_img' => $this->request->getVar('apps_sub_category_banner_img'),
+                'updated_at' => date("Y-m-d H:i:s"),
+                'updated_by' => session()->get('users_email')
+            ]);
+            session()->setFlashdata('successMsg', $this->updatedSuccessMsg);
+            return redirect()->to('admin/apps-documentation');
+        } catch (\Throwable $th) {
+            $this->logErrorModel->InsertLog(
+                $this->router->controllerName(),
+                $this->router->methodName(),
+                $th,
+                session()->get('users_email')
+            );
+            session()->setFlashdata('errorMsg', $this->errorProcessingMsg);
+            return redirect()->to('admin/apps-documentation');
+        }
+    }
+
+    public function Active($apps_sub_category_pid = null)
+    {
+        try {
+            $apps_sub_category = $this->appsSubCategoryModel
+                ->AppsSubCategoryWithAppsDetail($apps_sub_category_pid);
+            $this->appsSubCategoryModel->update($apps_sub_category_pid, [
+                'is_active'   => !$apps_sub_category->is_active,
+                'updated_at'  => date('Y-m-d H:i:s'),
+                'updated_by'  => session()->get('users_email')
+            ]);
+            $apps_sub_category_name = $apps_sub_category->apps_sub_category_title  . ' - ' . $apps_sub_category->apps_name . ' ' . $apps_sub_category->apps_subname;
+            $message = $apps_sub_category->is_active ? $apps_sub_category_name . ' successfully disabled' : $apps_sub_category_name . ' successfully enabled';
+            return redirect()->back()->with('successMsg', $message);
+        } catch (\Throwable $th) {
+            $this->logErrorModel->InsertLog(
+                $this->router->controllerName(),
+                $this->router->methodName(),
+                $th,
+                session()->get('users_email')
+            );
+            session()->setFlashdata('errorMsg', $this->errorProcessingMsg);
+            return redirect()->to('admin/apps-documentation');
         }
     }
 }
