9ab6f808670fd0af3d2cb79b82c49029aafeb375
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-09-25 11:42:50 +0700

    add validation jquery and php

--- app/Views/admin/apps/apps_input_view.php
+++ app/Views/admin/apps/apps_input_view.php
@@ -2,6 +2,31 @@
 
 <?= $this->section('custom_css'); ?>
 <link rel="stylesheet" href="<?= base_url('assets/css/page-knowledge-base.min.css'); ?>">
+<link rel="stylesheet" href="<?= base_url('assets/css/form-validation.css'); ?>">
+<?= $this->endSection(); ?>
+
+<?= $this->section('custom_js'); ?>
+<script src="<?= base_url('assets/js/jquery.validate.min.js'); ?>"></script>
+<script>
+    $(function() {
+        'use strict';
+        if ($('#frm_apps').length) {
+            $('#frm_apps').validate({
+                rules: {
+                    'apps_name': {
+                        required: true
+                    },
+                    'apps_owner': {
+                        required: true
+                    },
+                    'apps_url': {
+                        required: true
+                    },
+                }
+            });
+        }
+    });
+</script>
 <?= $this->endSection(); ?>
 
 <?= $this->section('content'); ?>
@@ -9,7 +34,7 @@
     <div class="col-12">
         <div class="card">
             <div class="card-body p-3">
-                <form class="form form-horizontal" action="<?= base_url('admin/apps-management/insert'); ?>" method="post" enctype="multipart/form-data">
+                <form id="frm_apps" name="frm_apps" class="form form-horizontal" action="<?= base_url('admin/apps-management/insert'); ?>" method="post" enctype="multipart/form-data">
                     <?= csrf_field(); ?>
                     <div class="row">
                         <div class="col-12">
@@ -18,7 +43,7 @@
                                     <label class="col-form-label text-black" for="apps_name">App Title</label>
                                 </div>
                                 <div class="col-sm-9">
-                                    <input type="text" id="apps_name" class="form-control" name="apps_name" placeholder="App title" required>
+                                    <input type="text" id="apps_name" class="form-control" name="apps_name" placeholder="App title" value="<?= old('apps_name'); ?>" required>
                                 </div>
                             </div>
                         </div>
@@ -28,7 +53,7 @@
                                     <label class="col-form-label text-black" for="apps_subname">App Subtitle</label>
                                 </div>
                                 <div class="col-sm-9">
-                                    <input type="text" id="apps_subname" class="form-control" name="apps_subname" placeholder="App subtitle">
+                                    <input type="text" id="apps_subname" class="form-control" name="apps_subname" placeholder="App subtitle" value="<?= old('apps_subname'); ?>">
                                 </div>
                             </div>
                         </div>
@@ -38,7 +63,7 @@
                                     <label class="col-form-label text-black" for="apps_desc">App Description</label>
                                 </div>
                                 <div class="col-sm-9">
-                                    <textarea id="apps_desc" name="apps_desc" class="form-control" placeholder="App description"></textarea>
+                                    <textarea id="apps_desc" name="apps_desc" class="form-control" placeholder="App description"><?= old('apps_desc'); ?></textarea>
                                 </div>
                             </div>
                         </div>
@@ -50,10 +75,15 @@
                                 <div class="col-sm-3">
                                     <select class="form-select" id="apps_owner" name="apps_owner">
                                         <option value="">- Choose owner -</option>
-                                        <option value="IT">IT</option>
-                                        <option value="HRGA">HRGA</option>
-                                        <option value="Marketing">Marketing</option>
-                                        <option value="Legal">Legal</option>
+                                        <option value="Accounting" <?= old('apps_owner') == "Accounting" ? 'selected' : null ?>>Accounting</option>
+                                        <option value="Audit" <?= old('apps_owner') == "Audit" ? 'selected' : null ?>>Audit</option>
+                                        <option value="Business Development" <?= old('apps_owner') == "Business Development" ? 'selected' : null ?>>Business Development</option>
+                                        <option value="Design" <?= old('apps_owner') == "Design" ? 'selected' : null ?>>Design</option>
+                                        <option value="Human Resource" <?= old('apps_owner') == "Human Resource" ? 'selected' : null ?>>Human Resource</option>
+                                        <option value="Information Technology" <?= old('apps_owner') == "Information Technology" ? 'selected' : null ?>>Information Technology</option>
+                                        <option value="Marketing" <?= old('apps_owner') == "Marketing" ? 'selected' : null ?>>Marketing</option>
+                                        <option value="Product" <?= old('apps_owner') == "Product" ? 'selected' : null ?>>Product</option>
+                                        <option value="Sales" <?= old('apps_owner') == "Sales" ? 'selected' : null ?>>Sales</option>
                                     </select>
                                 </div>
                             </div>
@@ -84,7 +114,17 @@
                                     <label class="col-form-label text-black" for="contact-info">App Icon</label>
                                 </div>
                                 <div class="col-sm-6">
-                                    <input id="apps_icon" name="apps_icon" class="form-control" type="file" id="formFile">
+                                    <input id="apps_icon" name="apps_icon" class="form-control <?= $validation->hasError('apps_icon') ? 'is-invalid' : null; ?>" type="file">
+                                    <div class="invalid-feedback"><?= $validation->getError('apps_icon'); ?></div>
+                                    <div class="alert alert-warning font-small-3 mt-1" role="alert">
+                                        <div class="alert-body">
+                                            <ul class="m-0">
+                                                <li>File format: SVG/PNG</li>
+                                                <li>Recommended size: 100x100 px</li>
+                                                <li>Max file size: 100kb</li>
+                                            </ul>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -94,7 +134,16 @@
                                     <label class="col-form-label text-black" for="contact-info">App Banner</label>
                                 </div>
                                 <div class="col-sm-6">
-                                    <input id="apps_banner_img" name="apps_banner_img" class="form-control" type="file" id="formFile">
+                                    <input id="apps_banner_img" name="apps_banner_img" class="form-control" type="file">
+                                    <div class="alert alert-warning font-small-3 mt-1" role="alert">
+                                        <div class="alert-body">
+                                            <ul class="m-0">
+                                                <li>File format: JPG/JPEG/PNG</li>
+                                                <li>Recommended size: 1440x400 px</li>
+                                                <li>Max file size: 200kb</li>
+                                            </ul>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
