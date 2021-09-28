81b96d07c7769365088b3e3c4876d90a6bdd9ef2
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-09-28 14:40:37 +0700

    add sop documents list

--- app/Views/user/sop_documents/sop_documents_view.php
+++ app/Views/user/sop_documents/sop_documents_view.php
@@ -10,7 +10,7 @@
         <div class="col-12">
             <div class="card knowledge-base-bg" style="background-image: url('<?= base_url('assets/img/banner/banner_sop.jpg'); ?>')">
                 <div class="card-body">
-                    <h1 class="text-black" style="font-size: 56px;">SOP Documents</h1>
+                    <h1 class="text-black" style="font-size: 56px;"><?= $title; ?></h1>
                 </div>
             </div>
         </div>
@@ -21,106 +21,47 @@
     <div class="row">
         <div class="col-lg-12">
             <ul class="nav nav-pills">
-                <li class="nav-item">
-                    <a class="nav-link active" id="home-tab" data-bs-toggle="pill" href="#home" aria-expanded="true">Category #1</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" id="profile-tab" data-bs-toggle="pill" href="#profile" aria-expanded="false">Category #2</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" id="about-tab" data-bs-toggle="pill" href="#about" aria-expanded="false">Category #3</a>
-                </li>
+                <?php
+                foreach ($sop_category as $key) { ?>
+                    <li class="nav-item">
+                        <a class="nav-link active" id="<?= strtolower(str_replace(' ', '-', $key['sop_category_title'])); ?>-tab" data-bs-toggle="pill" href="#<?= strtolower(str_replace(' ', '-', $key['sop_category_title'])); ?>" aria-expanded="true"><?= $key['sop_category_title']; ?></a>
+                    </li>
+                <?php } ?>
             </ul>
         </div>
     </div>
 
-    <div class="row pt-2">
-        <div class="col-lg-6 col-md-6 col-sm-12">
-            <div class="card">
-                <div class="card-body">
-                    <div>
-                        <h4 class="card-title text-black">Documentation #1</h4>
-                        <p class="card-text">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi laborum
-                            corporis dolores animi, veniam inventore. Repellendus fuga voluptatibus
-                            magni autem eum blanditiis facilis, nisi ipsam ducimus doloremque corporis,
-                            placeat odit similique sapiente! Ipsum enim fuga voluptatibus eligendi velit
-                            ratione laboriosam blanditiis reiciendis facilis itaque! Quod consequuntur
-                            voluptate maxime error cum.
-                        </p>
-                        <a href="./document.html" class="btn btn-outline-primary waves-effect">Open
-                            Document</a>
-                    </div>
-                </div>
+    <div class="tab-content">
+        <?php
+        foreach ($sop_category as $key) { ?>
+            <div role="tabpanel" class="tab-pane active" id="<?= strtolower(str_replace(' ', '-', $key['sop_category_title'])); ?>" aria-labelledby="<?= strtolower(str_replace(' ', '-', $key['sop_category_title'])); ?>-tab" aria-expanded="true">
+                <?php
+                foreach ($sop_documents as $key2) {
+                    if ($key2['sop_category_pid'] == $key['sop_category_pid']) { ?>
+                        <div class="row pt-2">
+                            <div class="col-lg-6 col-md-6 col-sm-12">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <div>
+                                            <h4 class="card-title text-black"><?= $key2['sop_documents_title']; ?></h4>
+                                            <p class="card-text">
+                                                <?= $key2['sop_documents_desc']; ?>
+                                            </p>
+                                            <a href="<?= base_url('user/sop-documents/detail/' . $key2['sop_documents_pid']); ?>" class="btn btn-outline-primary waves-effect">Open Document</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                    <?php
+                    }
+                }
+                    ?>
+                        </div>
+                    <?php } ?>
             </div>
-        </div>
-
-
-        <div class="col-lg-6 col-md-6 col-sm-12">
-            <div class="card">
-                <div class="card-body">
-                    <div>
-                        <h4 class="card-title text-black">Documentation #1</h4>
-                        <p class="card-text">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi laborum
-                            corporis dolores animi, veniam inventore. Repellendus fuga voluptatibus
-                            magni autem eum blanditiis facilis, nisi ipsam ducimus doloremque corporis,
-                            placeat odit similique sapiente! Ipsum enim fuga voluptatibus eligendi velit
-                            ratione laboriosam blanditiis reiciendis facilis itaque! Quod consequuntur
-                            voluptate maxime error cum.
-                        </p>
-                        <a href="./document.html" class="btn btn-outline-primary waves-effect">Open
-                            Document</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-
-        <div class="col-lg-6 col-md-6 col-sm-12">
-            <div class="card">
-                <div class="card-body">
-                    <div>
-                        <h4 class="card-title text-black">Documentation #1</h4>
-                        <p class="card-text">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi laborum
-                            corporis dolores animi, veniam inventore. Repellendus fuga voluptatibus
-                            magni autem eum blanditiis facilis, nisi ipsam ducimus doloremque corporis,
-                            placeat odit similique sapiente! Ipsum enim fuga voluptatibus eligendi velit
-                            ratione laboriosam blanditiis reiciendis facilis itaque! Quod consequuntur
-                            voluptate maxime error cum.
-                        </p>
-                        <a href="./document.html" class="btn btn-outline-primary waves-effect">Open
-                            Document</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-
-        <div class="col-lg-6 col-md-6 col-sm-12">
-            <div class="card">
-                <div class="card-body">
-                    <div>
-                        <h4 class="card-title text-black">Documentation #1</h4>
-                        <p class="card-text">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi laborum
-                            corporis dolores animi, veniam inventore. Repellendus fuga voluptatibus
-                            magni autem eum blanditiis facilis, nisi ipsam ducimus doloremque corporis,
-                            placeat odit similique sapiente! Ipsum enim fuga voluptatibus eligendi velit
-                            ratione laboriosam blanditiis reiciendis facilis itaque! Quod consequuntur
-                            voluptate maxime error cum.
-                        </p>
-                        <a href="./document.html" class="btn btn-outline-primary waves-effect">Open
-                            Document</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-
     </div>
 
-    <div class="row">
+    <!-- <div class="row">
         <div class="d-flex justify-content-center">
             <ul class="pagination mt-2">
                 <li class="page-item prev-item"><a class="page-link" href="#"></a></li>
@@ -136,6 +77,6 @@
                 <li class="page-item next-item"><a class="page-link" href="#"></a></li>
             </ul>
         </div>
-    </div>
+    </div> -->
 </section>
 <?= $this->endSection(); ?>
