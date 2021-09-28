e2b082bb6ddcfe7b555b0a1c878f276c42cb4e75
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-09-28 13:38:28 +0700

    mod document dynamic

--- app/Views/user/apps/apps_detail_view.php
+++ app/Views/user/apps/apps_detail_view.php
@@ -9,13 +9,13 @@
 <section id="knowledge-base-search">
     <div class="row">
         <div class="col-12">
-            <div class="card knowledge-base-bg" style="background-image: url('<?= base_url('assets/img/banner/' . $apps['apps_banner_img']); ?>')">
+            <div class="card knowledge-base-bg" style="background-image: url('<?= base_url('assets/uploads/banners/' . $apps['apps_banner_img']); ?>')">
                 <div class="card-body">
                     <div class="row">
                         <div class="text-end col-lg-3">
-                            <h1 class="text-black" style="font-size: 56px;"><?= $apps['apps_subname']; ?></h1>
+                            <h1 class="text-black" style="font-size: 56px;"><?= $apps['apps_name']; ?></h1>
                             <p class="d-inline card-text mb-2 text-black">
-                                <span><?= $apps['apps_name']; ?></span>
+                                <span><?= $apps['apps_subname']; ?></span>
                             </p>
                         </div>
                     </div>
@@ -42,93 +42,23 @@
     <div class="row pt-3">
         <h2 class="text-black pb-1 text-center text-center">Documentations</h2>
 
-        <div class="col-lg-6 col-md-6 col-sm-12">
-            <div class="card">
-                <div class="card-body">
-                    <div>
-                        <h4 class="card-title text-black">Documentation #1</h4>
-                        <p class="card-text">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi laborum
-                            corporis dolores animi, veniam inventore. Repellendus fuga voluptatibus
-                            magni autem eum blanditiis facilis, nisi ipsam ducimus doloremque corporis,
-                            placeat odit similique sapiente! Ipsum enim fuga voluptatibus eligendi velit
-                            ratione laboriosam blanditiis reiciendis facilis itaque! Quod consequuntur
-                            voluptate maxime error cum.
-                        </p>
-                        <a href="<?= base_url('User/Documentations/detail/1'); ?>" class="btn btn-outline-primary waves-effect">Open
-                            Document</a>
+        <?php
+        foreach ($apps_documentation as $key) { ?>
+            <div class="col-lg-6 col-md-6 col-sm-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div>
+                            <h4 class="card-title text-black"><?= $key['apps_document_title']; ?></h4>
+                            <p class="card-text">
+                                <?= $key['apps_document_desc']; ?>
+                            </p>
+                            <a href="<?= base_url('user/apps-documentations/detail/' . $key['apps_document_pid']); ?>" class="btn btn-outline-primary waves-effect">Open Document</a>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-
-
-        <div class="col-lg-6 col-md-6 col-sm-12">
-            <div class="card">
-                <div class="card-body">
-                    <div>
-                        <h4 class="card-title text-black">Documentation #1</h4>
-                        <p class="card-text">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi laborum
-                            corporis dolores animi, veniam inventore. Repellendus fuga voluptatibus
-                            magni autem eum blanditiis facilis, nisi ipsam ducimus doloremque corporis,
-                            placeat odit similique sapiente! Ipsum enim fuga voluptatibus eligendi velit
-                            ratione laboriosam blanditiis reiciendis facilis itaque! Quod consequuntur
-                            voluptate maxime error cum.
-                        </p>
-                        <a href="<?= base_url('User/Documentations/detail/1'); ?>" class="btn btn-outline-primary waves-effect">Open
-                            Document</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-
-        <div class="col-lg-6 col-md-6 col-sm-12">
-            <div class="card">
-                <div class="card-body">
-                    <div>
-                        <h4 class="card-title text-black">Documentation #1</h4>
-                        <p class="card-text">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi laborum
-                            corporis dolores animi, veniam inventore. Repellendus fuga voluptatibus
-                            magni autem eum blanditiis facilis, nisi ipsam ducimus doloremque corporis,
-                            placeat odit similique sapiente! Ipsum enim fuga voluptatibus eligendi velit
-                            ratione laboriosam blanditiis reiciendis facilis itaque! Quod consequuntur
-                            voluptate maxime error cum.
-                        </p>
-                        <a href="<?= base_url('User/Documentations/detail/1'); ?>" class="btn btn-outline-primary waves-effect">Open
-                            Document</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-
-        <div class="col-lg-6 col-md-6 col-sm-12">
-            <div class="card">
-                <div class="card-body">
-                    <div>
-                        <h4 class="card-title text-black">Documentation #1</h4>
-                        <p class="card-text">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi laborum
-                            corporis dolores animi, veniam inventore. Repellendus fuga voluptatibus
-                            magni autem eum blanditiis facilis, nisi ipsam ducimus doloremque corporis,
-                            placeat odit similique sapiente! Ipsum enim fuga voluptatibus eligendi velit
-                            ratione laboriosam blanditiis reiciendis facilis itaque! Quod consequuntur
-                            voluptate maxime error cum.
-                        </p>
-                        <a href="<?= base_url('User/Documentations/detail/1'); ?>" class="btn btn-outline-primary waves-effect">Open
-                            Document</a>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <?php } ?>
 
     </div>
-
-    <p class="text-center fw-bolder text-primary pt-4">
-        <a href="#">Show More</a>
-    </p>
 </section>
 <?= $this->endSection(); ?>
