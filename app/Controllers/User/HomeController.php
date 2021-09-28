ed5f178df9564e5b7d62dfd0cf90cc97a7881b32
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-09-28 11:50:59 +0700

    query union list documents

--- app/Controllers/User/HomeController.php
+++ app/Controllers/User/HomeController.php
@@ -21,7 +21,8 @@ class HomeController extends BaseController
             'apps'  => $this->appsModel
                 ->where('is_active', true)
                 ->get()
-                ->getResult('array')
+                ->getResult('array'),
+            'documents' => db_connect()->query("select * from v_documents")->getResultArray()
         ];
         return view('user/home/home_view', $data);
     }
